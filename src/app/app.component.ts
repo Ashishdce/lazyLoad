ae9610858931ef6886bfa7ed990c4ad5b21b3725
Ashish Jangid <ajangi@sapient.com>
2017-05-19 18:15:25 +0530

    code added

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import {Component,HostListener,NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import HelloWorldComponent from './components/dyanamic-content/hello-world-component';
+import WorldHelloComponent from './components/dyanamic-content/world-hello-component';
+import FooterComponent from './components/dyanamic-content/footer';
+import DynamicComponent from './components/dyanamic-content/dyanamic-content.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app works!';
+  componentData1 = null;
+  componentData2 = null;
+   componentData3 = null;
+  i = 0;
+ @HostListener("window:scroll", [])
+// onWindowScroll(){
+//         this.componentData2 = {
+//         component: HelloWorldComponent,
+//         inputs: {
+//           showNum: 9
+//         }
+//     };
+//     this.componentData3 = {
+//         component: FooterComponent,
+//         inputs: {
+//         showNum: 2
+//         }
+//     };
+//     this.componentData1 = {
+//         component: WorldHelloComponent,
+//         inputs: {
+//           showNum: 2
+//         }
+//     };
+    
+// }
+
+handlerFunction(num){
+    if(num===1){
+        this.componentData1 = {
+        component: WorldHelloComponent,
+        inputs: {
+          showNum: 2
+        }
+    };
+    }
+    else if(num===2){
+         this.componentData2 = {
+        component: HelloWorldComponent,
+        inputs: {
+          showNum: 9
+        }
+    };
+    }
+    else{
+        this.componentData3 = {
+        component: FooterComponent,
+        inputs: {
+        showNum: 2
+        }
+    };
+    }
+}
+  
+}    
+  
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ViewportModule } from 'angular2-viewport';
+import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import { AppComponent } from './app.component';
+
+import {RouterModule} from '@angular/router';
+
+import { GetUserDataService } from './services/user-data/user-data.service';
+import HelloWorldComponent from './components/dyanamic-content/hello-world-component';
+import WorldHelloComponent from './components/dyanamic-content/world-hello-component';
+import DynamicComponent from './components/dyanamic-content/dyanamic-content.component';
+import FooterComponent from './components/dyanamic-content/footer';
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    ViewportModule
+  ],
+  providers: [GetUserDataService],
+  declarations: [ AppComponent,FooterComponent, HelloWorldComponent, WorldHelloComponent, DynamicComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
